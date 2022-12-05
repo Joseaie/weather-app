4f6cf358d358a0f1c2567e5224d26bb439a35908
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2022-12-05 12:00:54 +0000

    Add DayCard to WeekGrid

--- src/components/weekGrid/WeekGrid.js
+++ src/components/weekGrid/WeekGrid.js
@@ -1,5 +1,24 @@
 import './WeekGrid.css';
+import DayCard from '../dayCard/DayCard';
 
-function WeekGrid() {}
+function WeekGrid({ location, days }) {
+  return (
+    <>
+      <p>{location}</p>
+      {days.map((day, index) => {
+        return (
+          <DayCard
+            key={index}
+            date={day.date}
+            day={day.day}
+            description={day.description}
+            imgSrc={day.imgSrc}
+            temperature={day.temperature}
+          />
+        );
+      })}
+    </>
+  );
+}
 
 export default WeekGrid;
--- src/components/weekGrid/WeekGrid.test.js
+++ src/components/weekGrid/WeekGrid.test.js
@@ -1,10 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import WeekGrid from './WeekGrid';
+import Wind from '../../assets/icons/wind.svg';
+import HeavyRain from '../../assets/icons/cloud-showers-heavy.svg';
 
 describe('WeekGrid', () => {
-  test('it renders the day of the week', () => {
-    render(<WeekGrid />);
-    const dayOfTheWeek = screen.getByText('Saturday');
-    expect(dayOfTheWeek).toBeInTheDocument();
+  const mockDays = [
+    {
+      date: 'March 2nd',
+      day: 'Monday',
+      description: 'light rain',
+      imgSrc: Wind,
+      temperature: '36',
+    },
+    {
+      date: 'March 3rd',
+      day: 'Tueseday',
+      description: 'heavy rain',
+      imgSrc: HeavyRain,
+      temperature: '20',
+    },
+  ];
+
+  test('it renders the location', () => {
+    render(<WeekGrid location='London, UK' days={mockDays} />);
+    const location = screen.getByText('London, UK');
+    expect(location).toBeInTheDocument();
+  });
+
+  test('it renders day cards', () => {
+    render(<WeekGrid location='London, UK' days={mockDays} />);
+    screen.getByText('Monday');
+    screen.getByText('Tueseday');
   });
 });
