6a4ebb33e2277afd6835eae1ea5c23c30ffaa858
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2022-12-06 14:39:06 +0000

    Style WeekGrid

--- src/components/weekGrid/WeekGrid.js
+++ src/components/weekGrid/WeekGrid.js
@@ -3,21 +3,23 @@ import DayCard from '../dayCard/DayCard';
 
 function WeekGrid({ location, days }) {
   return (
-    <>
-      <p>{location}</p>
-      {days.map((day, index) => {
-        return (
-          <DayCard
-            key={index}
-            date={day.date}
-            day={day.day}
-            description={day.description}
-            imgSrc={day.imgSrc}
-            temperature={day.temperature}
-          />
-        );
-      })}
-    </>
+    <div className='week-grid'>
+      <p className='location grey-text'>{location}</p>
+      <div className='days-container'>
+        {days.map((day, index) => {
+          return (
+            <DayCard
+              key={index}
+              date={day.date}
+              day={day.day}
+              description={day.description}
+              imgSrc={day.imgSrc}
+              temperature={day.temperature}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
